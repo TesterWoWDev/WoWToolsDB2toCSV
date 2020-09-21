f2bd2a4ed944dd947260a24e5a72c61449f31122
Connor <connorkilbourne@gmail.com>
2020-09-21 14:31:26 -0400

    added a spell sql update to work with some russian dbc

---
+++ src/mm.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class mm {
+    public static void main(String[] args) throws IOException
+    {
+        FileWriter spellSQL = new FileWriter("SpellSQL.csv");
+        try (BufferedReader br = new BufferedReader(new FileReader("spell.csv"))) {
+            String line;
+            br.readLine();//skip header
+            while ((line = br.readLine()) != null) {
+                String[] displayRow = line.split(",");
+                System.out.println(Arrays.toString(displayRow));
+                if(isParsable(displayRow[0].replace("\"","")))
+                spellSQL.write("UPDATE `item_template` SET `Name_lang` = '" + displayRow[1].replace("\"","") + "', `Description_lang = '" + displayRow[3].replace("\"","") + "' WHERE `ID` = " +  displayRow[0].replace("\"","") + ";\n");
+            }
+        }
+        spellSQL.close();
+
+    }
+    public static boolean isParsable(String input) {
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch (final NumberFormatException e) {
+            return false;
+        }
+    }
+}
